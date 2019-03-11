cad38da28d9f6e055fa92e62f08a085def47735a
gali k <gbmatrix@gmail.com>
2019-03-11 13:11:10 +0000

    added postgres instead of sqlite3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # config.action_cable.allowed_request_origins = ['https://ruby2-6-gbmatrix.c9users.io/'] # address needs to be changed to production
+
 end
